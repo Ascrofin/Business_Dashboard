9d329ad87eba0f47537d6e66064f1d3394be5af3
Andres Ignacio Scrofina <73431468+Ascrofin@users.noreply.github.com>
2023-07-30 19:37:18 -0300

    updated uri

--- server/index.js
+++ server/index.js
@@ -29,9 +29,9 @@ app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
 app.use("/transaction", transactionRoutes);
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
