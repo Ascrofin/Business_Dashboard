4f5709f9b43c8621ae5336e3f5bada6c2414ad1a
Andres Ignacio Scrofina <73431468+Ascrofin@users.noreply.github.com>
2023-07-30 21:51:11 -0300

    added

--- src/state/api.ts
+++ src/state/api.ts
@@ -6,7 +6,7 @@ import {
 } from "./types";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://business-dashboard-gucj.vercel.app "}),
   reducerPath: "main",
   tagTypes: ["Kpis", "Products", "Transactions"],
   endpoints: (build) => ({
